9ef912a4522b3155d6a48097795fc172a15cec1b
Lalit Bhosale <lalit.bhosale27@gmail.com>
2026-09-21 18:11:51 +0900

    Initial

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_personal_godou_GodouApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.personal.godou.GodouApp"
-)
-public final class _com_personal_godou_GodouApp {
-}
--- app/build/generated/ksp/debug/resources/META-INF/proguard/com_personal_godou_ui_settings_ThemeViewModel_HiltModules_BindsModule_LazyClassKeys.pro
+++
@@ -1,1 +0,0 @@
--keep,allowobfuscation,allowshrinking class com.personal.godou.ui.settings.ThemeViewModel
